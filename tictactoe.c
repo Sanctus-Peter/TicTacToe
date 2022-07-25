b5fde3670955140cf548cc4060b1397fb51571aa
sanctuspeter@gmail.com <s4nctuspeter@gmail.com>
2022-07-25 13:32:49 +0100

    removed global variables and made a few changes

--- tictactoe.c
+++ tictactoe.c
@@ -1,3 +1,10 @@
+/**
+ * tictactoe - A game of tictactoe to be played between 2 players
+ *
+ * Created by: Sanctus-Peter
+ * cc: 1st July, 2022.
+ *
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -8,48 +15,52 @@
 #define draw 0
 #define playOn -1
 
-char nSquare[row][col] = {
-	{'1','2','3'},
-	{'4','5','6'},
-	{'7','8','9'}
-};
+void board(char nSquare[row][col]);
+int checkWin(char nSquare[row][col]);
 
-void board();
-int checkWin(void);
+/**
+ * main - main function to start the game
+ * Return: 0 if successful, 1 otherwise
+ */
 
-int main (void)
+int main(void)
 {
+	char nSquare[row][col] = {
+		{'1', '2', '3'},
+		{'4', '5', '6'},
+		{'7', '8', '9'}
+	};
 	int player = 1;
 	int choice, chkValid = playOn;
 	char var;
 
-	while(chkValid == playOn)
+	while (chkValid == playOn)
 	{
-		board();
-		player = (player % 2)? 1 : 2;
+		board(nSquare);
+		player = (player % 2) ? 1 : 2;
 		printf("Player %d, take turn, Enter a valid number: ",
 				player);
 		scanf("%d", &choice);
 
-		var = (player == 1)? 'X' : 'O';
-		
-		if(choice == 1 && nSquare[0][0] == '1')
+		var = (player == 1) ? 'X' : 'O';
+
+		if (choice == 1 && nSquare[0][0] == '1')
 			nSquare[0][0] = var;
-		else if(choice == 2 && nSquare[0][1] == '2')
+		else if (choice == 2 && nSquare[0][1] == '2')
 			nSquare[0][1] = var;
-		else if(choice == 3 && nSquare[0][2] == '3')
+		else if (choice == 3 && nSquare[0][2] == '3')
 			nSquare[0][2] = var;
-		else if(choice == 4 && nSquare[1][0] == '4')
+		else if (choice == 4 && nSquare[1][0] == '4')
 			nSquare[1][0] = var;
-		else if(choice == 5 && nSquare[1][1] == '5')
+		else if (choice == 5 && nSquare[1][1] == '5')
 			nSquare[1][1] = var;
-		else if(choice == 6 && nSquare[1][2] == '6')
+		else if (choice == 6 && nSquare[1][2] == '6')
 			nSquare[1][2] = var;
-		else if(choice == 7 && nSquare[2][0] == '7')
+		else if (choice == 7 && nSquare[2][0] == '7')
 			nSquare[2][0] = var;
-		else if(choice == 8 && nSquare[2][1] == '8')
+		else if (choice == 8 && nSquare[2][1] == '8')
 			nSquare[2][1] = var;
-		else if(choice == 9 && nSquare[2][2] == '9')
+		else if (choice == 9 && nSquare[2][2] == '9')
 			nSquare[2][2] = var;
 		else
 		{
@@ -58,66 +69,76 @@ int main (void)
 			system("pause");
 		}
 
-		chkValid = checkWin();
+		chkValid = checkWin(nSquare);
 
 		player += 1;
 	}
 
-	board();
+	board(nSquare);
 
-	if(chkValid == win)
+	if (chkValid == win)
 		printf("  Congratulations!!! \a Player %d win\n\n", --player);
-	else 
+	else
 		printf("  Game ended in a draw\a\n\n");
-	
-//	system("pause");
-	
+
+
 	return (0);
 }
 
-void board(void)
+/**
+ * board - displays board on the console
+ * @nSquare: array of board placement
+ */
+
+void board(char nSquare[row][col])
 {
 	system("clear");
 	printf("\n\n\n\t   Tic Tac Toe\n\n\n");
 	printf("   Player 1 (X) - player 2 (O)\n\n\n");
 	printf("\t     |     |     \n");
 	printf("\t  %c  |  %c  |  %c  \n",
-			nSquare[0][0],nSquare[0][1],nSquare[0][2]);
+			nSquare[0][0], nSquare[0][1], nSquare[0][2]);
 	printf("\t_____|_____|_____\n");
 	printf("\t     |     |     \n");
 	printf("\t  %c  |  %c  |  %c  \n",
-			nSquare[1][0],nSquare[1][1],nSquare[1][2]);
+			nSquare[1][0], nSquare[1][1], nSquare[1][2]);
 	printf("\t_____|_____|_____\n");
 	printf("\t     |     |     \n");
 	printf("\t  %c  |  %c  |  %c  \n",
-			nSquare[2][0],nSquare[2][1],nSquare[2][2]);
+			nSquare[2][0], nSquare[2][1], nSquare[2][2]);
 	printf("\t     |     |     \n\n");
 }
 
-int  checkWin(void)
-{
-	if(nSquare[0][0] == nSquare[0][1] && nSquare[0][1] == nSquare[0][2])
-		return win;
-	else if(nSquare[1][0] == nSquare[1][1] && nSquare[1][1] == nSquare[1][2])
-		return win;
-	else if(nSquare[2][0] == nSquare[2][1] && nSquare[2][1] == nSquare[2][2])
-		return win;
-	else if(nSquare[0][1] == nSquare[1][1] && nSquare[1][1] == nSquare[2][1])
-		return win;
-	else if(nSquare[0][0] == nSquare[1][0] && nSquare[1][0] == nSquare[2][0])
-		return win;
-	else if(nSquare[0][2] == nSquare[1][2] && nSquare[1][2] == nSquare[2][2])
-		return win;
-	else if(nSquare[0][0] == nSquare[1][1] && nSquare[1][1] == nSquare[2][2])
-		return win;
-	else if(nSquare[0][2] == nSquare[1][1] && nSquare[2][0] == nSquare[1][1])
-		return win;
-	else if(nSquare[0][0] != '1' && nSquare[0][1] != '2' && nSquare[0][2] != '3' 
-			&& nSquare[1][0] != '4' && nSquare[1][1] != '5' 
-			&& nSquare[1][2] != '6' && nSquare[2][0] != '7' 
-			&& nSquare[2][1] != '8' && nSquare[2][2] != '9')
-		return draw;
-	else
-		return playOn;
+/**
+ * checkWin - checks if the game is over and if there is a win
+ * @nSquare: array position of the board
+ *
+ * Return: 1 if there is a win, 0 if there is a draw
+ */
 
+int  checkWin(char nSquare[row][col])
+{
+	if (nSquare[0][0] == nSquare[0][1] && nSquare[0][1] == nSquare[0][2])
+		return (win);
+	else if (nSquare[1][0] == nSquare[1][1] && nSquare[1][1] == nSquare[1][2])
+		return (win);
+	else if (nSquare[2][0] == nSquare[2][1] && nSquare[2][1] == nSquare[2][2])
+		return (win);
+	else if (nSquare[0][1] == nSquare[1][1] && nSquare[1][1] == nSquare[2][1])
+		return (win);
+	else if (nSquare[0][0] == nSquare[1][0] && nSquare[1][0] == nSquare[2][0])
+		return (win);
+	else if (nSquare[0][2] == nSquare[1][2] && nSquare[1][2] == nSquare[2][2])
+		return (win);
+	else if (nSquare[0][0] == nSquare[1][1] && nSquare[1][1] == nSquare[2][2])
+		return (win);
+	else if (nSquare[0][2] == nSquare[1][1] && nSquare[2][0] == nSquare[1][1])
+		return (win);
+	else if (nSquare[0][0] != '1' && nSquare[0][1] != '2' && nSquare[0][2] != '3'
+			&& nSquare[1][0] != '4' && nSquare[1][1] != '5'
+			&& nSquare[1][2] != '6' && nSquare[2][0] != '7'
+			&& nSquare[2][1] != '8' && nSquare[2][2] != '9')
+		return (draw);
+	else
+		return (playOn);
 }
